1c098e08d33fda6cd596c2f75c87df5203b58dca
Sergey <xxsmotur@gmail.com>
2018-11-29 23:49:29 +0500

    init for examples

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/graphql-go/graphql"
+)
+
+// User defines ident of the user
+type User struct {
+	ID          string       `json:"id,omitempty"`
+	Firstname   string       `json:"firstName"`
+	Lastname    string       `json:"lastName"`
+	Phone       string       `json:"phone"`
+	Gender      string       `json:"gender"`
+	Attachments []Attachment `json:"attachments"`
+	CreatedDate time.Time
+}
+
+// Attachment defines additional link to attached info
+type Attachment struct {
+	ID   string `json:"id,omitempty"`
+	Link string `json:"link"`
+}
+
+func main() {
+	schema, _ := graphql.NewSchema(graphql.SchemaConfig{})
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := graphql.Do(graphql.Params{
+			Schema:        schema,
+			RequestString: r.URL.Query().Get("query"),
+		})
+		json.NewEncoder(w).Encode(result)
+	})
+	http.ListenAndServe(":8080", nil)
+}
